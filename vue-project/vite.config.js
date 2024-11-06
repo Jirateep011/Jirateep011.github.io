9e0f315234ac26f760f55b7250c531de605dd4ea
Jirateep011 <6431501011@lamduan.mfu.ac.th>
2024-11-07 06:47:28 +0700

    done1

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Jirateep011.github.io/', // เพิ่มบรรทัดนี้
   plugins: [
     vue(),
   ],
